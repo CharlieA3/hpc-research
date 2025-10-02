d3f1e13bb14ff7454c451c7dd866aa67454d297c
Charlie <amante.charlie@gmail.com>
2025-10-02 00:44:55 -0400

    File structure modified

---
+++ include/llm_profile.hpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <vector.h>
+#include <string.h>
+
+
+using namespace std;
+
+class Profiler {
+public:
+	
+	Profiler();
+
+	void run_model(string model);
+
+private:
+	
+
+};	
